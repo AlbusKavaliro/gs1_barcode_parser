f36d78b4c08cc141e0ad517f6f99546bdfc04a02
hangy <hangy@hangy.de>
2026-08-17 23:15:07 +0200

    test: Remove unused imports from issue_16_test.dart

--- test/issue_16_test.dart
+++ test/issue_16_test.dart
@@ -1,6 +1,4 @@
 import 'package:gs1_barcode_parser/gs1_barcode_parser.dart';
-import 'package:gs1_barcode_parser/src/ai.dart';
-import 'package:gs1_barcode_parser/src/exception.dart';
 import 'package:test/test.dart';
 
 main() {
